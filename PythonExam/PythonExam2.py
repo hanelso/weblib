d383b1a02bfe15698540c16c4e5e2ff25685ec10
hanelso <gjsxj1992@gmail.com>
2018-09-21 21:37:29 +0900

    파이썬 과제 완료 -파이썬 과제 2번 수정

--- PythonExam/PythonExam2.py
+++ PythonExam/PythonExam2.py
@@ -6,16 +6,18 @@ while True:
     n = random.randrange(ma) + 1
 
     print("수를 결정하였습니다. 맞추어 보세요", n)
-    print(mi, "-", ma)
+
 
     while True:
-
+        print(mi, "-", ma)
         val = int(input(str(trycount) +">>"))
 
         if val > n:
             print("더 낮게")
+            ma = val-1
         elif val < n :
             print("더 높게")
+            mi = val+1
         else:
             print("맞았습니다.")
             break
