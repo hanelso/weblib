560d7d2948ebbde1d60930a40f611deb9760c85a
hanelso <gjsxj1992@gmail.com>
2018-09-21 15:54:26 +0900

    기본 서버 뼈대 제작 2 -> 완결

--- myhttpserver2.py
+++ myhttpserver2.py
@@ -1,3 +1,7 @@
+# 장고를 쓰기 전에 웹서버 간단한 프레임 워크를 진행했다.
+# 웹 서버의 기능을 해주는 뼈대는 이렇게 되어있다.
+# 일단 익숙해 지기 전에 이것을 분석하는것은 위험...
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
 
@@ -10,18 +14,39 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         params = parse_qs(qs)
         values = params.get(name)
 
-        return values.pop()
+        return "" if values is None else values.pop()
+        #같은 의미의 구문.
+        # if values is None:
+        #     return None
+        # else:
+        #     return values.pop()
+    def ex1(self):
+        self.send_response(200)  # 서버에서 응답해주는 부분
+        self.send_header("Content-Type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("<h1>EX1 Fuction</h1>".encode("utf-8"))
+
+    def ex2(self):
+        self.send_response(200)  # 서버에서 응답해주는 부분
+        self.send_header("Content-Type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("<h1>EX2 Fuction</h1>".encode("utf-8"))
 
     def do_GET(self):
-        req_url = self.path[:self.path.find('?')]
+        index = self.path.find('?')
+        req_url = self.path if index == -1 else self.path[:index]
+
 
         #URL MAPPING
-        if req_url=='/iot':
-            value = self.get_params('ex')
-            self.send_response(200)     #서버에서 응답해주는 부분
-            self.send_header("Content-Type","text/html; charset=utf-8")
-            self.end_headers()
-            self.wfile.write("<h1>TEST</h1>".encode("utf-8"))
+        if req_url == "/iot":
+            handler_name = 'ex' + self.get_params('ex')
+        #    print(handler_name)
+            if handler_name not in MyHTTPRequestHandler.__dict__:
+                self.send_error(404,"File Not Found")
+                return
+
+            MyHTTPRequestHandler.__dict__[handler_name](self)
+
         elif req_url == '/board':
             pass
         else:
