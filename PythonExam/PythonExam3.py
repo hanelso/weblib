4bd7a2cba50ea9623ff95e6e83a74fb3f9fc3066
hanelso <gjsxj1992@gmail.com>
2018-09-23 00:30:37 +0900

    파이썬 과제 -파이썬 과제 3번 수정 완료

--- PythonExam/PythonExam3.py
+++ PythonExam/PythonExam3.py
@@ -1,10 +1,10 @@
 
 def PrintCost(menu):
     str = input("메뉴: ")
-    if str == '오뎅': #오뎅 가격이 반값이라...
-        return int(menu[str]/2)
+    # if str == '오뎅': #오뎅 가격이 600일때...
+    #     return int(menu[str]/2)
     return menu[str]
 
-menu = {"오뎅":600,"순대":800,"만두":600}
+menu = {"오뎅":300,"순대":800,"만두":600}
 # print(menu)
 print("가격: " + str(PrintCost(menu)))
