fa95be2a0620b7bceb5b92ebe0a2e0f3ee54d13e
hanelso <gjsxj1992@gmail.com>
2018-09-27 09:31:11 +0900

    파이썬 과제 -1번 최종수정2 -1번을 다듬기 제출용

--- PythonExam/PythonExam1.py
+++ PythonExam/PythonExam1.py
@@ -9,23 +9,33 @@ original = 	'''<html>
 </html>
 '''
 
-plist = original.splitlines()
-#print(plist)
-index = len(plist)
+plist = original.splitlines()           # html 문자열을 '\n'를 기준으로 라인을 나눠 list로 만든다.
 
-while index != 0:
-    str = plist[len(plist)-index]
-    str1 = ""
+for str in plist:
 
-    if str.find('<') == -1:
-        str1 = str
+    if str.find('<') == -1:             # '<' 이 없는 문자열은 그대로 출력
+        str2 = str
     else:
-        str1 += str[:str.find('<')]
-        str2 = str[str.find('>')+1:]
-        str1 += str2[:str2.find('<')]
-        str1 = str1.strip(" " "\t")
+        str1 = str[str.find('>')+1:]    # '>'문자룰 찾고 그 이후의 문자열을 str1에 저장
+        str2 = str1[:str1.find('<')]    # str1에서 '<'문자열 이후로 전부 버림
 
-    if str1 != "":
-        print(str1)
+    if str2 != "":
+        print(str2)
 
-    index -= 1
+
+#print(plist)                           # list가 잘 생겼는지 확인
+
+# index = len(plist)                      #while 문의 반복횟수
+# while index != 0:
+#     str = plist[len(plist)-index]
+#
+#     if str.find('<') == -1:             # '<' 이 없는 문자열은 그대로 출력
+#         str2 = str
+#     else:
+#         str1 = str[str.find('>')+1:]    # '>'문자룰 찾고 그 이후의 문자열을 str1에 저장
+#         str2 = str1[:str1.find('<')]    # str1에서 '<'문자열 이후로 전부 버림
+#
+#     if str2 != "":
+#         print(str2)
+#
+#     index -= 1
